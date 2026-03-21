bbc1eb3dbf9257cf0cc179514cdcebb49c50b10c
kauablima <dev.kaualima@gmail.com>
2026-03-21 19:48:15 -0300

    case resolved(princesa v1)

--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,5 +1,44 @@
 package main
+
 import "fmt"
+
+func handlePrint(vetor []int, swordIdx int) {
+	fmt.Print("[")
+	for i, val := range vetor {
+		if i == swordIdx {
+			fmt.Printf(" %v>", val)
+		} else {
+			fmt.Printf(" %v", val)
+		}
+	}
+	fmt.Println(" ]")
+}
+
 func main() {
-    fmt.Println("qxcode")
+	var num, player int
+	fmt.Scan(&num, &player)
+
+	vetor := make([]int, num)
+	for i := 0; i < num; i++ {
+		vetor[i] = i + 1
+	}
+
+	swordIdx := 0
+	for i, v := range vetor {
+		if v == player {
+			swordIdx = i
+			break
+		}
+	}
+
+	for len(vetor) > 1 {
+		handlePrint(vetor, swordIdx)
+
+		killIdx := (swordIdx + 1) % len(vetor)
+
+		vetor = append(vetor[:killIdx], vetor[killIdx+1:]...)
+
+		swordIdx = killIdx % len(vetor)
+	}
+	handlePrint(vetor, swordIdx)
 }
