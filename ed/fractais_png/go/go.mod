9df15408c49705536693519aca779382ff0f77b7
kauablima <dev.kaualima@gmail.com>
2026-03-31 17:06:49 -0300

    case resolved(triangulo)

---
+++ ed/fractais_png/go/go.mod
@@ -0,0 +1,3 @@
+module pen.go
+
+go 1.22.2
---
+++ ed/triangulo/go/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func processa(vet []int) {
+	//vetor := append([]int(nil), vet...)
+
+	if len(vet) > 1 {
+		arr := []int{}
+
+		for i := 0; i < len(vet)-1; i++ {
+			arr = append(arr, vet[i]+vet[i+1])
+		}
+
+		processa(arr)
+
+	}
+
+	fmt.Printf("[ %v ]\n", Join(vet, " "))
+	// 1. defina o ponto de parada
+	// 2. monte o vetor auxiliar com os resultados das somas
+	// 3. chame recursivamente a função processa para o vetor auxiliar
+	// 4. imprima o vetor original
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	if !scanner.Scan() {
+		return
+	}
+	line := scanner.Text()
+	parts := strings.Fields(line)
+	vet := []int{}
+	for _, part := range parts {
+		if value, err := strconv.Atoi(part); err == nil {
+			vet = append(vet, value)
+		}
+	}
+	processa(vet)
+}
+
+func Join[T any](v []T, sep string) string {
+	if len(v) == 0 {
+		return ""
+	}
+	s := ""
+	for i, x := range v {
+		if i > 0 {
+			s += sep
+		}
+		s += fmt.Sprintf("%v", x)
+	}
+	return s
+}
