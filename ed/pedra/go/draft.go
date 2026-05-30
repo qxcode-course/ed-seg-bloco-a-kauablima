394906044bdedc89a11a591199aa5709acc16859
kauablima <dev.kaualima@gmail.com>
2026-05-30 03:00:29 -0300

    case(pedra)

--- ed/pedra/go/draft.go
+++ ed/pedra/go/draft.go
@@ -1,20 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func myJoin(lista []int, sep string) string {
-	saida := ""
+type Players struct {
+	p1 float64
+	p2 float64
+}
 
-	return saida
+func Champion(vet []Players) int {
+	winner := -1
+	var count float64
+
+	for i := 0; i < len(vet); i++ {
+		if vet[i].p1 >= 10 && vet[i].p2 >= 10 {
+			difference := math.Abs(vet[i].p1 - vet[i].p2)
+
+			if winner == -1 || difference < count {
+				count = difference
+				winner = i
+			}
+		}
+	}
+
+	return winner
 }
 
 func main() {
 	var num int
 	fmt.Scan(&num)
 
+	vet := make([]Players, num)
 	for i := 0; i < num; i++ {
-		var n1, n2 int
-		fmt.Scan(&n1, &n2)
+		fmt.Scan(&vet[i].p1, &vet[i].p2)
+	}
 
+	result := Champion(vet)
+	if result != -1 {
+		fmt.Println(Champion(vet))
+	} else {
+		fmt.Println("sem ganhador")
 	}
 }
