c362f9694f48014d36fc67ac8641438b9b26ecb2
kauablima <dev.kaualima@gmail.com>
2026-06-02 11:11:28 -0300

    case(subset_sum)

--- ed/subset_sum/src/go/draft.go
+++ ed/subset_sum/src/go/draft.go
@@ -1,5 +1,30 @@
 package main
 import "fmt"
+
+
+func search(vet []int, idx, curSum int, sum int, count int) bool {
+    if curSum == sum {
+        return true
+    }
+
+    if count == len(vet) || curSum > sum {
+        return false 
+    }
+
+    nextIdx := (idx+1)%len(vet)
+
+    return search(vet, nextIdx, curSum+vet[idx], sum, count+1 ) ||
+            search(vet, nextIdx, curSum, sum, count+1)
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+    var num, sum int
+    fmt.Scan(&num, &sum)
+
+    vet := make([]int, num)
+    for i := range num {
+        fmt.Scan(&vet[i])
+    }
+
+    fmt.Println(search(vet, 0, 0, sum, 0))
 }
