f5601213835c86712e17aae07cf039f5612a4777
kauablima <dev.kaualima@gmail.com>
2026-07-06 22:59:53 -0300

    case(fila): chaves

--- ed/chaves/src/go/main.go
+++ ed/chaves/src/go/main.go
@@ -1,7 +1,27 @@
 package main
 
-import (
-)
+import "fmt"
 
 func main() {
+	queue := NewQueue[string]()
+
+	for i := 'A'; i <= 'P'; i++ {
+		queue.Enqueue(string(i))
+	}
+
+	for i := 0; i < 15; i++ {
+		t1 := queue.Dequeue()
+		t2 := queue.Dequeue()
+
+		var g1, g2 int
+		fmt.Scan(&g1, &g2)
+
+		if g1 > g2 {
+			queue.Enqueue(t1)
+		} else {
+			queue.Enqueue(t2)
+		}
+	}
+	
+	fmt.Println(queue.Dequeue())
 }
